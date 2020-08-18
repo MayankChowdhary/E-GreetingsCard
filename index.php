5d17a1145534b07a432ccd9a5f6fcd70b9a88e81
MayankChowdhary <mayankchoudhary00@gmail.com>
2020-08-19 00:42:41 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,231 @@
+<?xml version="1.0" encoding="UTF-8"?> <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+     <?php
+    if (isset($_GET['n'])){
+       
+        $name = $_GET['n'];
+    }
+     
+else
+     {
+        
+        $name= 'Sanam';
+    }
+    ?>
+  <head>
+     
+      <link rel="shortcut icon" href="img/heart.png" type="image/png" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0,user-scalable=0, initial-scale=1.0, shrink-to-fit=no" />
+    <meta name="description" content="In the Remembrance of Love" />
+    <meta name="author" content="Mayank Choudhary" />
+
+    <title >Valentine's Day Live Greeting Card</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/one-page-wonder.css" rel="stylesheet" />
+      <style type="text/css">
+             .layer:after{content:'<?php echo $name;?>';}</style>
+</head>
+   
+
+  <body>
+      
+      <main >
+      <div class="container" >
+        <div class="row align-items-center">
+          <div class="col-lg-6 order-lg-2">
+               
+              <h1 class="card-title center-align comic wishesed center animated pulse">
+                      <span class="red-text text-lighten-1" >I&nbsp;</span>
+                      <span class="black-text text-accent-4">L</span>
+                      <span class="yellow-accent-text text-lighten-1">o</span>
+                      <span class="blue-text text-accent-3">v</span>
+                      <span class="red-text text-accent-3">e&nbsp;</span>
+                      <!--<span class="pink-text">n</span>
+                      <span class="black-text text-accent-3">g &nbsp;</span>-->
+                      <span class="blue-text">Y</span>
+                      <span class="black-text text-lighten-1">o</span>
+                      <span class="red-text text-accent-3">u</span>
+                    </h1>
+            <div class="p-5" >
+              <img class=" sizem  rounded-circle" src="img/rose.gif" alt="Blooming_red_rose.gif" />
+            </div>
+              <div class="p-4">
+               <img class="img-fluid " src="img/violin_girl.gif" alt="Violin Girl" /><br/><br/>
+                  <marquee class="m22 animated pulse" behavior="scroll" direction="up" scrolldelay="3" scrollamount="1" ><br/>
+                <p>I love your dark eyes,<br/>And your curly hair,<br />I love your smile,<br />And the way that you care.<br/>I love your deep kisses,<br />I love your soft touch,<br />I love you, I love you,<br />I love you so much.<br />Happy Valentine's Day!</p>
+          </marquee>
+              </div>
+              <marquee class="m8" behavior="scroll" direction="left" scrolldelay="0" scrollamount="6" ><br />
+             <img class="img-fluid" alt="bike.gif"  src="img/bike.gif" height="140px" width="150px" /><br />
+        </marquee>
+               <marquee class="m3" behavior="scroll" direction="left" scrolldelay="0" scrollamount="10" ><br />
+             <img  alt="bird.gif"  src="img/bird0.gif" height="120px" width="120px"/>
+        </marquee>
+         
+             <!-- <marquee class="m9" behavior="scroll" direction="left" scrolldelay="1" scrollamount="10">Copyright &copy; MC-Developers&reg; 2018.All Rights Reserved.
+                  </marquee> -->
+              <form method="GET" action="ready.php">
+	<div class="enter-name">
+		<input class="animated pulse " type="name" required="" maxlength="50" name="n" placeholder="👉Lover Name Here">
+        <button  class="btnn animatedd shake" type="submit"><span>👉</span>Go</button>
+	</div>
+</form>
+              
+             
+            </div>
+          </div> 
+          
+          <div class="stage">
+        
+     <div class="layer"></div>   
+      <div class="layer"></div> 
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div> 
+        
+        </div>
+          <marquee class="m4" behavior="scroll" direction="right" scrolldelay="1" scrollamount="6" ><br />
+             <img src="img/bird1.gif" alt="Bird.gif" height="120px" width="120px"/>
+        </marquee>
+          </div>
+          </main>
+           
+               
+            
+            
+             
+            
+            
+    
+          
+<marquee class="m1" behavior="scroll" direction="up" scrolldelay="5">  <br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+</marquee>
+<marquee class="m2" behavior="scroll" direction="down" scrolldelay="5"><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+</marquee>
+          
+       
+          
+         <!--<div class="col-lg-6 order-lg-1">
+            <div class="p-5">
+                 
+            </div>
+          </div>-->
+          
+          <audio  id="music" loop="loop" preload="auto" oncanplaythrough="mymusic"> 
+              <source src="img/back_tone.mp3"  type="audio/mpeg" />Mohabbatein tone</audio>
+       <div class="back_music ">
+    <h6 id="question" >Do you want to play background Music?</h6>
+            <img class="img-fluid"  id="wave" src="img/bars.gif">
+      <button id="welcome" onclick="mymusic()" >Yes</button>
+           <button id="cancel" onclick="nomusic()" >No</button>
+    </div>
+    
+      
+ <script>
+     function mymusic(){
+    var  x=document.getElementById("music");
+             x.play();
+        var dth= document.getElementsByClassName("back_music");
+         for(var i=0; i<dth.length; i++){
+             
+             dth[i].style.visibility= "hidden";
+         }
+         
+     }
+     function nomusic(){
+        var dth= document.getElementsByClassName("back_music");
+         for(var i=0; i<dth.length; i++){
+             
+             dth[i].style.visibility= "hidden";
+     }
+    }
+    
+      </script>
+
+    <!-- Footer -->
+      
+   <footer class="py-56"  >
+        
+         
+             <!-- <marquee class="m9" behavior="scroll" direction="left" scrolldelay="1" scrollamount="10">Copyright &copy; MC-Developers&reg; 2018.All Rights Reserved.
+                  </marquee> 
+
+ <p class="m-0 text-center text-white small"> Copyright &copy; MC-Developers&trade; 2018</p>-->
+         <marquee class="mm" behavior="scroll" direction="left" scrolldelay="1" scrollamount="10">Copyright &copy; MC-Developers&reg; 2018.All Rights Reserved.
+             </marquee>
+    </footer> 
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+       
+  </body>
+
+</html>
---
+++ ready.php
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="UTF-8"?> <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+     <?php
+    if (isset($_GET['n'])){
+       
+        //$name = $_GET['n'];
+        $name = preg_replace('/\s+/', '', $_GET['n']);
+    }
+     
+else
+     {
+        
+        $name= 'Sanam';
+    }
+$url = "https://mc-developers.000webhostapp.com/?n=".$name;
+
+  $fulltext= $name." here is your special Valentines Day Live Greetings Card: 👉 ";
+    ?>
+  <head>
+     
+      <link rel="shortcut icon" href="img/heart.png" type="image/png" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, maximum-scale=1,user-scalable=no, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="In the Remembrance of Love" />
+    <meta name="author" content="Mayank Choudhary" />
+
+    <title >Valentine's Day Live Greeting Card</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/one-page-wonder.css" rel="stylesheet" />
+    
+      <style type="text/css">
+             .layer:after{content:'<?php echo $name;?>';}</style>
+</head>
+   
+
+  <body>
+      
+      <main>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 order-lg-2">
+               
+              <h1 class="card-title center-align comic wishesed center animated pulse">
+                      <span class="red-text text-lighten-1" >I&nbsp;</span>
+                      <span class="black-text text-accent-4">L</span>
+                      <span class="yellow-accent-text text-lighten-1">o</span>
+                      <span class="blue-text text-accent-3">v</span>
+                      <span class="red-text text-accent-3">e&nbsp;</span>
+                      <!--<span class="pink-text">n</span>
+                      <span class="black-text text-accent-3">g &nbsp;</span>-->
+                      <span class="blue-text">Y</span>
+                      <span class="black-text text-lighten-1">o</span>
+                      <span class="red-text text-accent-3">u</span>
+                    </h1>
+            <div class="p-5">
+              <img class="sizem rounded-circle" src="img/rose.gif" alt="Blooming_red_rose.gif" />
+            </div>
+              <div class="p-4">
+               <img class="img-fluid " src="img/violin_girl.gif" alt="Violin Girl" /><br/><br/>
+                  <marquee class="m22 animated pulse" behavior="scroll" direction="up" scrolldelay="3" scrollamount="1" ><br/>
+                <p>I love your dark eyes,<br/>And your curly hair,<br />I love your smile,<br />And the way that you care.<br/>I love your deep kisses,<br />I love your soft touch,<br />I love you, I love you,<br />I love you so much.<br />Happy Valentine's Day!</p>
+          </marquee>
+              </div>
+              <marquee class="m8" behavior="scroll" direction="left" scrolldelay="0" scrollamount="6" ><br />
+             <img class="img-fluid" alt="bike.gif"  src="img/bike.gif" height="140px" width="150px" /><br />
+        </marquee>
+               <marquee class="m3" behavior="scroll" direction="left" scrolldelay="0" scrollamount="10" ><br />
+             <img  alt="bird.gif"  src="img/bird0.gif" height="120px" width="120px"/>
+        </marquee>
+        
+    
+            
+              
+             <div class="whatsapp">
+     <a href="whatsapp://send?text=<?php echo $name.' here is your special Valentines Live Greetings Card:'. PHP_EOL .' 👉  '.$url ?>" class="wa_btn animated pulse">&nbsp;&nbsp;Share On Whatsapp</a>
+</div>
+
+              
+          <div class="shareany">
+              <button  onclick="shareall()" class="sa_btn animated pulse">Share on Any App&nbsp;&nbsp;&nbsp;&nbsp;</button> 
+                   
+        </div>     
+              
+        </div>
+          </div> 
+          
+          <div class="stage">
+        
+     <div class="layer"></div>   
+      <div class="layer"></div> 
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div> 
+        
+        </div>
+          <marquee class="m4" behavior="scroll" direction="right" scrolldelay="1" scrollamount="6" ><br />
+             <img src="img/bird1.gif" height="120px" width="120px"/>
+        </marquee>
+          
+          
+
+          </div>
+          </main>
+           
+               
+            
+            
+             
+            
+            
+    
+          
+<marquee class="m1" behavior="scroll" direction="up" scrolldelay="5">  <br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+</marquee>
+<marquee class="m2" behavior="scroll" direction="down" scrolldelay="5"><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+<img src="img/flag5.gif" height="40px" width="40px"/><br /><br />
+</marquee>
+          
+         
+          
+         <!--<div class="col-lg-6 order-lg-1">
+            <div class="p-5">
+                 
+            </div>
+          </div>-->
+          
+           <audio  id="music" loop="loop" preload="auto" oncanplaythrough="mymusic"> 
+              <source src="img/back_tone.mp3"  type="audio/mpeg" />Mohabbatein tone</audio>
+       <div class="back_music ">
+    <h6 id="question" >Play background Music Again?</h6>
+          <img  id="wave" src="img/bars.gif">
+      <button id="welcome" onclick="mymusic()" >Yes</button>
+           <button id="cancel" onclick="nomusic()" >No</button>
+    </div>
+      <script>
+     function mymusic(){
+    var  x=document.getElementById("music");
+             x.play();
+        var dth= document.getElementsByClassName("back_music");
+         for(var i=0; i<dth.length; i++){
+             
+             dth[i].style.visibility= "hidden";
+         }
+         
+     }
+     function nomusic(){
+        var dth= document.getElementsByClassName("back_music");
+         for(var i=0; i<dth.length; i++){
+             
+             dth[i].style.visibility= "hidden";
+     }
+     }
+          
+    
+          
+     function shareall() {
+         
+         
+         
+         if(navigator.share) {
+          navigator.share({
+            title: 'Valentines Day Live Greetings Card',
+            text: '<?php echo $fulltext; ?>',
+            url: '<?php echo $url; ?>',
+
+          })
+          .then(() => console.log('Share complete'))
+          .error((error) => console.error('Could not share at this time', error))
+        } else{
+            
+        var textx =' <?php echo $fulltext.$url; ?>';
+        var dummy = document.createElement("input");
+
+      // Add it to the document
+      document.body.appendChild(dummy);
+
+      // Set its ID
+      dummy.setAttribute("id", "dummy_id");
+
+      // Output the array into it
+      document.getElementById("dummy_id").value=textx;
+
+      // Select it
+      dummy.select();
+
+      // Copy its contents
+      document.execCommand("copy");
+            document.body.removeChild(dummy);
+            
+            alert('It seems that You are not using Android Chrome Browser So This Link is Copied to your Clipboard just paste it anywhare you want!')
+        }
+        
+  }
+          
+      </script>
+
+    <footer class="py-56"  >
+        
+         
+             <!-- <marquee class="m9" behavior="scroll" direction="left" scrolldelay="1" scrollamount="10">Copyright &copy; MC-Developers&reg; 2018.All Rights Reserved.
+                  </marquee> 
+
+ <p class="m-0 text-center text-white small"> Copyright &copy; MC-Developers&trade; 2018</p>-->
+         <marquee class="mm" behavior="scroll" direction="left" scrolldelay="1" scrollamount="10">Copyright &copy; MC-Developers&reg; 2018.All Rights Reserved.
+             </marquee>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      
+  </body>
+
+</html>
